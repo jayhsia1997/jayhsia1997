9b0c63cd12b5be3ec13752a253770e5092de40e4
Jay Hsia <jayhsia1997@gmail.com>
2025-06-10 17:59:32 -0400

    feat(main): Remove notification components from Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "@/components/home/Hero.tsx";
 import Skills from "@/components/home/Skills.tsx";
-import Notification from "@/components/ui/notification/Notfication.tsx";
 
 
 const Home: React.FC = () => {
@@ -9,10 +8,6 @@ const Home: React.FC = () => {
     <>
       <Hero/>
       <Skills/>
-      <Notification variant="success" title="Success" description="This is a success notification"/>
-      <Notification variant="info" title="Info" description="This is an info notification"/>
-      <Notification variant="warning" title="Warning" description="This is a warning notification"/>
-      <Notification variant="error" title="Error" description="This is an error notification"/>
     </>
   );
 };
